344e477a8303669f7214011cf75b4bb934938084
Steven Moyano <stevenmoyano1@gmail.com>
2021-03-03 13:59:57 -0600

    completed tasks pending last

--- main.js
+++ main.js
@@ -10,7 +10,11 @@
 
 console.log('##### Challenge 1 #####');
 // 1.
-//
+    for (let i = 0; i <= 20; i++) {
+        if (i % 3 === 0) {
+            console.log(i);
+        }
+    }
 // Console out the numbers from 1-20, but only if they're divisible by 3.
 // Hint: Use `if` and a modulus operation.
 
@@ -18,18 +22,21 @@ console.log('##### Challenge 1 #####');
 
 console.log('##### Challenge 2 #####');
 // 2.
-//
+    for (let i = 20; i > 0; i--) {
+        console.log(i);
+    }
 // Console out the numbers from 1-20. BACKWARDS.
 
 
 
 console.log('##### Challenge 3 #####');
 // 3.
-//
 // For the string 'Boy howdy am I good at this!', console out the letters
 // individually.
 let string3 = 'Boy howdy am I good at this!';
-
+for (const letter of string3) {
+    console.log(letter);
+}
 
 
 console.log('##### Challenge 4 #####');
@@ -38,6 +45,11 @@ console.log('##### Challenge 4 #####');
 // For the string, 'And getting better every day.', starting with the second
 // character, print out every third character.
 let string4 = 'And getting better every day.';
+let newStr = "";
+for (let i = 1; i < string4.length; i += 3) {
+    newStr += string4[i];
+    console.log(newStr);
+}
 
 
 
@@ -47,6 +59,11 @@ console.log('##### Challenge 5 #####');
 // For the string 'Am I the best?', console out the letters individually, AND
 // UPPERCASED.
 let string5 = 'Am I the best?';
+for (const letter of string5) {
+    console.log(letter.toUpperCase());
+}
+
+
 
 
 
@@ -56,6 +73,16 @@ console.log('##### Challenge 6 #####');
 // For the string 'Whoa, I am the best!', console out the letters individually,
 // but also capitalize the letters in the third word.
 let string6 = 'Whoa, I am the best!';
+let spaces = "";
+for (let value of string6) {
+    if (value === " ") {
+        spaces += value;
+    } else if (spaces.length === 2) {
+        value = value.toUpperCase();
+    }
+    console.log(value);
+}
+
 
 
 
@@ -65,14 +92,27 @@ console.log('##### Challenge 7 #####');
 // For the string 'I am become death, destroyer of worlds.', console out the letters
 //  individually, but backwards. That is, the first console log should be
 // `.`, followed by `s`, followed by `d`, and so on.
-let string7 = 'I am become death, destroyer of worlds.';
-
+// let string7 = 'I am become death, destroyer of worlds.';
+// for (let i = string7.length - 1; i >= 0; i--) {
+//     console.log(string7[i]);
+// }
 
 
 console.log('##### Challenge 8 #####');
 // 8.
 //
 const names = 'Alex|Mesuara|Brian|Chanel|Matt|Jeremy B.|Jose|Andy|Dustin|Joshua|Steve H.|Cesar|Nastya|Jeremy L.|Michael|John|Lee|Ruth|Steve M.|Tim|Ronny|Stevie R|Neo|Rick|Kyle|Minahil|Adam|Sonny|James|Daisy|Willie';
+console.log(names);
+let emptystr = "";
+let stored = "";
+for (let value of names) {
+    if (value === "|") {
+        console.log(stored);
+        stored = "";
+    } else {
+        stored += value;
+    }
+}
 
 // Console out the individual letters of each person in class, which are
 // in the string above. Every time you start a new name (including the first
